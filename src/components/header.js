aef934b699d9bd3ef52c6464e7339ced3826559c
Lokin Crook <lcrook@paulding.k12.ga.us>
2018-05-08 16:29:22 -0400

    added hogwarts data

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../images/logo.svg';
-
+import Crest from '../images/Crest.png';
 
 // functional component assigned to header var
 const Header = () => {
     return (
     <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={Crest} className="App-logo" alt="logo" />
             <h1 className="App-title">Safety Native App 2.0</h1>
     </header>
     );
